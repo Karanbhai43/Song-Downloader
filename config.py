e315b4894f8d67e1ad8e39c32c4e0556b7aa6c13
Karanbhai43 <134896321+Karanbhai43@users.noreply.github.com>
2023-05-30 14:30:53 +0530

    Update config.py
    Signed-off-by: Karanbhai43 <134896321+Karanbhai43@users.noreply.github.com>

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config(object):
-    DATABASE = os.environ.get("DB_URI")
+    DATABASE = os.environ.get("mongodb+srv://Karan43:<password>@cluster0.dybhptl.mongodb.net/?retryWrites=true&w=majority")
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1961162381").split())
     SUPPORT = os.environ.get("SUPPORT")
